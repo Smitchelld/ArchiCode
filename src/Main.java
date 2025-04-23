76cd59787e2a8da7599bfc273e44d0d85a15f4e7
xWerve <148155508+xWerve@users.noreply.github.com>
2025-04-23 22:13:23 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,30 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        CharStream input = CharStreams.fromFileName("input.txt"); // lub z konsoli
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.out.println("Użycie: java Main <plik_z_programem.ArchC>");
+            return;
+        }
+
+        CharStream input = CharStreams.fromFileName(args[0]);
         ArchiCodeLexer lexer = new ArchiCodeLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         ArchiCodeParser parser = new ArchiCodeParser(tokens);
+
+        parser.removeErrorListeners();
+        parser.addErrorListener(new DiagnosticErrorListener());
+
+        if (parser.getNumberOfSyntaxErrors() > 0) {
+            System.out.println("Błąd składniowy! Program nie został wykonany.");
+            return;
+        }
+
         ParseTree tree = parser.program();
-
         ArchiCodeInterpreter interpreter = new ArchiCodeInterpreter();
-        interpreter.isDeclarationPass = true;
-        interpreter.visit(tree); // Pass 1 - declarations
-
-        interpreter.isDeclarationPass = false;
-        interpreter.visit(tree); // Pass 2 - execution
+        interpreter.visit(tree);
     }
 }
