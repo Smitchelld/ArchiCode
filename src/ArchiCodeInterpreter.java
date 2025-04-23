d3d7547845d2f8ef76fe5faacab454a38a116a68
xWerve <148155508+xWerve@users.noreply.github.com>
2025-04-23 19:59:49 +0200

    Update ArchiCodeInterpreter.java

--- src/ArchiCodeInterpreter.java
+++ src/ArchiCodeInterpreter.java
@@ -1,51 +1,68 @@
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.*;
-
 import java.util.HashMap;
 import java.util.Map;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.io.IOException;
-import java.util.Scanner;
 
-public class ArchiCodeInterpreter {
-    private static final Map<String, Integer> variables = new HashMap<>();
+public class ArchiCodeInterpreter extends ArchiCodeBaseVisitor<Object> {
+    public Map<String, Object> globalVariables = new HashMap<>();
+    public boolean isDeclarationPass = true;
 
-    public static void main(String[] args) throws Exception {
-        String input = new String(Files.readAllBytes(Paths.get("program.ArchC")));
-
-        CharStream charStream = CharStreams.fromString(input);
-        ArchiCodeLexer lexer = new ArchiCodeLexer(charStream);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        ArchiCodeParser parser = new ArchiCodeParser(tokens);
-        parser.removeErrorListeners();
-        parser.addErrorListener(new BaseErrorListener() {
-            @Override
-            public void syntaxError(Recognizer<?, ?> recognizer,
-                                    Object offendingSymbol,
-                                    int line, int charPositionInLine,
-                                    String msg, RecognitionException e) {
-                System.err.printf("Błąd składni: linia %d:%d - %s%n", line, charPositionInLine, msg);
-                System.exit(1);
+    @Override
+    public Object visitAssignment(ArchiCodeParser.AssignmentContext ctx) {
+        String varName = ctx.ID().getText();
+        if (isDeclarationPass) {
+            if (globalVariables.containsKey(varName)) {
+                throw new RuntimeException("Redeclaration of variable: " + varName);
             }
-        });
-        ParseTree tree = parser.program();
-        ParseTreeWalker walker = new ParseTreeWalker();
-        ArchiCodeExecutor executor = new ArchiCodeExecutor();
-        walker.walk(executor, tree);
-    }
-}
-
-class ArchiCodeExecutor extends ArchiCodeBaseListener {
-
-    @Override
-    public void enterShowStatement(ArchiCodeParser.ShowStatementContext ctx) {
-        String text = ctx.expr().getText().replace("\"", "");
-        System.out.println(text);
+            globalVariables.put(varName, null);
+            return null;
+        } else {
+            if (!globalVariables.containsKey(varName)) {
+                throw new RuntimeException("Undeclared variable: " + varName);
+            }
+            Object value = visit(ctx.expression());
+            globalVariables.put(varName, value);
+            return null;
+        }
     }
 
     @Override
-    public void enterBlueprintStatement(ArchiCodeParser.BlueprintStatementContext ctx) {
+    public Object visitExprStatement(ArchiCodeParser.ExprStatementContext ctx) {
+        return visit(ctx.expression());
+    }
 
+    @Override
+    public Object visitExpression(ArchiCodeParser.ExpressionContext ctx) {
+        if (ctx.op != null) {
+            Object left = visit(ctx.expression(0));
+            Object right = ctx.expression().size() > 1 ? visit(ctx.expression(1)) : null;
+            switch (ctx.op.getText()) {
+                case "+": return (Double) left + (Double) right;
+                case "-": return (Double) left - (Double) right;
+                case "*": return (Double) left * (Double) right;
+                case "/": return (Double) left / (Double) right;
+                case "==": return left.equals(right);
+                case "!=": return !left.equals(right);
+                case "<": return (Double) left < (Double) right;
+                case ">": return (Double) left > (Double) right;
+                case "and": return (Boolean) left && (Boolean) right;
+                case "or": return (Boolean) left || (Boolean) right;
+            }
+        }
+
+        if (ctx.getChildCount() == 2 && ctx.getChild(0).getText().equals("not")) {
+            return !(Boolean) visit(ctx.expression(0));
+        }
+
+        if (ctx.NUMBER() != null) return Double.parseDouble(ctx.NUMBER().getText());
+        if (ctx.TRUE() != null) return true;
+        if (ctx.FALSE() != null) return false;
+        if (ctx.ID() != null) {
+            String varName = ctx.ID().getText();
+            if (!globalVariables.containsKey(varName)) {
+                throw new RuntimeException("Undeclared variable: " + varName);
+            }
+            return globalVariables.get(varName);
+        }
+
+        return visit(ctx.expression(0));
     }
 }
